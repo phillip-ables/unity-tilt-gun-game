6c90d98f51fd34c5f91298bfa03ff4b09479dd14
lip ables <phillipables1@gmail.com>
2019-01-04 20:45:49 -0500

    on start find type canvas group and set alpha to 1

--- tiltGunGame/Assets/Scripts/MenuSceneScript.cs
+++ tiltGunGame/Assets/Scripts/MenuSceneScript.cs
@@ -6,5 +6,10 @@ public class MenuSceneScript : MonoBehaviour {
     private CanvasGroup fadeGroup;
     private float fadeInSpeed = 0.33f; // three seconds
 
+    private void Start()
+    {
+        fadeGroup = FindObjectOfType<CanvasGroup>();
 
+        fadeGroup.alpha = 1;
+    }
 }
