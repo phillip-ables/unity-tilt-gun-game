7b2a671bbfb87f1da06ed35230a60a5e98519e08
lip ables <phillipables1@gmail.com>
2019-01-04 19:56:37 -0500

    onstart grab the only canvas group object and set the alpha to 1, then if time is less then min logo time then load time is equal to min logo tim, else loadtime = time

--- tiltGunGame/Assets/Scripts/PreLoaderScript.cs
+++ tiltGunGame/Assets/Scripts/PreLoaderScript.cs
@@ -6,4 +6,22 @@ public class PreLoaderScript : MonoBehaviour {
     private CanvasGroup fadeGroup;
     private float loadTime;
     private float minimumLogoTime = 3.0f;  // Minimum time of that scene
+
+    private void Start()
+    {
+        // Grab the only CanvasGroup in the scene
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        fadeGroup.alpha = 1;
+
+        //Preload the game
+        // $$
+
+        //appriciate the logo
+        if (Time.time < minimumLogoTime)
+            loadTime = minimumLogoTime;
+        else
+            loadTime = Time.time;
+
+    }
 }
