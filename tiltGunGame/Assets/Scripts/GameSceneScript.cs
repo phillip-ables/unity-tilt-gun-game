c0dcaca9b43692f19b8528e946add290916c327e
lip ables <phillipables1@gmail.com>
2019-01-08 18:57:12 -0500

    private vars for canvas group float for fade duration and a bool for game started

--- tiltGunGame/Assets/Scripts/GameSceneScript.cs
+++ tiltGunGame/Assets/Scripts/GameSceneScript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameSceneScript : MonoBehaviour {
+    private CanvasGroup fadeGroup;
+    private float fadInDuration = 2;
+    private bool gameStarted;
+    
+
     public void ExitScene()
     {
         SceneManager.LoadScene("MainMenu");
