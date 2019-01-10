a79586b975952f107d2b791b72b8b1025d700aa5
lip ables <phillipables1@gmail.com>
2019-01-10 01:39:12 -0500

    menu player script, update position forward at time

--- tiltGunGame/Assets/Scripts/MenuPlayerScript.cs
+++ tiltGunGame/Assets/Scripts/MenuPlayerScript.cs
@@ -3,5 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuPlayerScript : MonoBehaviour {
-    
+    private void Update()
+    {
+        transform.position += Vector3.forward * 3 * Time.deltaTime;
+    }
 }
