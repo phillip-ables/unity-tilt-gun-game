937410d90e7d8247f473e7e6d9876a58dd53361e
lip ables <phillipables1@gmail.com>
2019-01-09 23:45:15 -0500

    munu camera public transform for shop and level way point, vector3 for start position and desired position, quaternion for start rotation and desired rotation

---
+++ tiltGunGame/Assets/Scripts/MenuCameraScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraScript : MonoBehaviour {
+    public Transform shopWayPoint;
+    public Transform levelWayPoint;
+
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+
+    private Vector3 desiredPosition;
+    private Quaternion desiredRotation;
+}
