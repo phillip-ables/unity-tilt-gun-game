bde413d42d49ecaacaa093311d40adc4efd0a191
lip ables <phillipables1@gmail.com>
2019-01-10 03:13:10 -0500

    a bunch of stuff for touch input

--- tiltGunGame/Assets/Scripts/ManagerScript.cs
+++ tiltGunGame/Assets/Scripts/ManagerScript.cs
@@ -6,6 +6,8 @@ public class ManagerScript : MonoBehaviour
 {
     public bool isAccelerometer;
 
+    private Dictionary<int, Vector2> activeTouches = new Dictionary<int, Vector2>();
+
     private void Start()
     {
         isAccelerometer = SystemInfo.supportsAccelerometer;
@@ -20,8 +22,34 @@ public class ManagerScript : MonoBehaviour
             acc.y = acc.z;
             return acc;
         }
-            
+
+        //read all touches from the user
+        Vector3 r = Vector3.zero;
+        foreach(Touch touch in Input.touches)
+        {
+            // If we just started pressing
+            if(touch.phase == TouchPhase.Began)
+            {
+                activeTouches.Add(touch.fingerId, touch.position);
+            }
+            // If we remove our finger
+            else if(touch.phase == TouchPhase.Ended)
+            {
+                if (activeTouches.ContainsKey(touch.fingerId))
+                    activeTouches.Remove(touch.fingerId);
+            }
+            // finger not moving
+            //use delta from origional position
+            else
+            { 
+                float mag = 0;
+                r = (touch.position - activeTouches[touch.fingerId]);
+                mag = r.magnitude / 300;
+                r = r.normalized * mag;
+            }
+        }
+        return r;
     }
-    
+
 
 }
